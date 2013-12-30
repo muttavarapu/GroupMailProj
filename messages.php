964ff485f3b9dcc3a76ead2ae7b7b7f57dcef5ce
shri12491 <shri12491@gmail.com>
2013-12-30 22:50:04 +0530

    added viewDoctors page

--- messages.php
+++ messages.php
@@ -5,16 +5,18 @@ redirect_to($goto);}
 if(isset($_GET['msg'])){$msg[]=$_GET['msg'];}
 if($session_id){
 
-$query=mysql_query("SELECT id,subject, LEFT(message, 145) AS body,sent_on,recipients FROM messages WHERE d_id='$session_id'") or die("Could not check the session".mysql_error());}else{redirect_to('login.php?msg="error please login again!"');}
+$query=mysql_query("SELECT id,subject, LEFT(message, 145) AS body,sent_on,recipients FROM messages WHERE d_id='$session_id'") or die("Could not check the session".mysql_error());}
+else{redirect_to('login.php?msg="error please login again!"');}
 
-$output= "<p class='msg'>You havet sent any messages</p>";
+if(mysql_num_rows($query)>0){$output="";
 while($row=mysql_fetch_array($query)){
 $readmore=$row['body'];
 $lastspace=strrpos($readmore," ");
 $read=substr($readmore,0,$lastspace);
-$output= "<div class='msg'><h2 class='subj'>Subj: ".$row['subject']."</h2><h2 class='date'>Sent On : ".$row['sent_on']."</h2><div class='clear'></div><p>".$read."...<a href='viewMessage.php?id=".$row['id']."'>View Message</a></p><h3>Recipients: ".$row['recipients']."</h3></div>";
+$output.= "<div class='msg'><h2 class='subj'>Subj: ".$row['subject']."</h2><h2 class='date'>Sent On : ".$row['sent_on']."</h2><div class='clear'></div><p>".$read."...<a href='viewMessage.php?id=".$row['id']."'>View Message</a></p><h3>Recipients: ".$row['recipients']."</h3></div>";
+}}
+else{$output= "<p class='msg'>You haven't sent any messages</p>";
 }
-
 
 include('includes/head.php');
 
