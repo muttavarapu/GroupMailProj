3b3b8883be9cf6c0d5f0a27cbc3fe33ca3d9eeb9
shri12491 <shri12491@gmail.com>
2014-01-01 21:49:34 +0530

    diagonize heroku-mysqli connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,6 @@
-<?php $connection = new mysqli("b13013ns7636sg5v.mysql.clvrcld.net","uhruoelhd77j0pf7","2c6c28c236794a56ae72ecf4e1128ba1","b13013ns7636sg5v");
+<?php 
+dl("mysqli.so");
+$connection = new mysqli("b13013ns7636sg5v.mysql.clvrcld.net","uhruoelhd77j0pf7","2c6c28c236794a56ae72ecf4e1128ba1","b13013ns7636sg5v");
 ini_set("error_reporting","E_ALL");
 if ($connection->connect_errno > 0) {
     die ('Unable to connect to database [' . $connection->connect_error . ']');
