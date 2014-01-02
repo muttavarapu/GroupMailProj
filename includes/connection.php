fc965784827bbf773cd9df12e60f1af22fdc5e5a
shri12491 <shri12491@gmail.com>
2014-01-02 21:53:58 +0530

    Host changed to fortrabbit

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,10 @@
 <?php 
-dl("mysqli.so");echo"what the fuck";
+
 $connection = new mysqli("b13013ns7636sg5v.mysql.clvrcld.net","uhruoelhd77j0pf7","2c6c28c236794a56ae72ecf4e1128ba1","b13013ns7636sg5v");
-ini_set("error_reporting","E_ALL");
-if ($connection->connect_errno > 0) {
-    die ('Unable to connect to database [' . $connection->connect_error . ']');
-}else{echo "connected";}?>
+if (mysqli_connect_error()) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+
+?>
